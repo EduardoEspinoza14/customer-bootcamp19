e4e82eff7934f93635296f3280e58b6db9cda8f1
EduardoEspinoza14 <eduardoantonio.espinozallontop@everis.nttdata.com>
2022-06-22 15:29:36 -0500

    Commit

--- src/main/java/com/nttdata/customer/controller/CustomerController.java
+++ src/main/java/com/nttdata/customer/controller/CustomerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -30,6 +31,11 @@ public class CustomerController {
 
   @Autowired
   CustomerService service;
+
+  @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Flux<CustomerMongo> getAllCustomer() {
+    return service.getCustomers();
+  }
 
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Mono<CustomerMongo> getCustomer(@PathVariable String id) {
